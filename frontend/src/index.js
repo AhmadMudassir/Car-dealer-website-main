5099ef13956dd6b2d224d18a4fb41103157aa5d8
AhmadMudassir548 <dev.ahmadmudassir@gmail.com>
2024-07-24 14:34:05 +0500

    Initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css'; // Ensure this CSS file exists or change the path if necessary
+import App from "./App"; // Ensure this component exists or create it
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
